f2e20a53e5a761c623ada40b7a2c4944a9b42ba0
Shawkat Hossain Maruf <shawkath646@gmail.com>
2024-09-21 21:54:13 +0600

    Fixed image config

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,5 +1,26 @@
+import { storage } from "@/config/firebase.config";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  NextResponse.json({ message: "Test api" }, { status: 200 });
+
+  /*
+
+  const fileName = "picture_xwnV6XZKPRa6421qdK9y.webp";
+
+  const fileRef = storage.bucket().file(fileName)
+
+  const [fileExists] = await fileRef.exists();
+
+  if (!fileExists) return NextResponse.json({ message: "File not found" }, { status: 404 });
+
+  await fileRef.rename("picture_xwnV6XZKPRa6421qdK9y");
+
+  const [downloadLink] = await fileRef.getSignedUrl({
+    action: 'read',
+    expires: '01-01-2074',
+  });
+
+  */
+
+  return NextResponse.json({ message: "Hi! This is test api" }, { status: 200 });
 }
