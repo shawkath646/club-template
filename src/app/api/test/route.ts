7484a878042f2365d01ac969d46b00adf8cc3185
Shawkat Hossain Maruf <shawkath646@gmail.com>
2024-10-01 23:11:13 +0600

    1 October 2024 Fixed

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -4,20 +4,20 @@ import fs from "fs";
 import path from "path";
 
 export async function GET() {
-  const filePath = path.join("C:/Users/Shawkat Hossain/Downloads", "picture_XCH7oGMGpUytmj71rLdl");
+  // const filePath = path.join("C:/Users/Shawkat Hossain/Downloads", "picture_XCH7oGMGpUytmj71rLdl");
   
-  const fileBuffer = fs.readFileSync(filePath);
+  // const fileBuffer = fs.readFileSync(filePath);
   
-  const base64Image = Buffer.from(fileBuffer).toString('base64');
+  // const base64Image = Buffer.from(fileBuffer).toString('base64');
   
-  const mimeType = "image/jpeg"; 
+  // const mimeType = "image/jpeg"; 
   
-  const base64ImageData = `data:${mimeType};base64,${base64Image}`;
+  // const base64ImageData = `data:${mimeType};base64,${base64Image}`;
 
-  const downloadUrl = await uploadFileToFirestore(base64ImageData, {
-    fileName: "picture_XCH7oGMGpUytmj71rLdl",
-    fileType: "image",
-  });
+  // const downloadUrl = await uploadFileToFirestore(base64ImageData, {
+  //   fileName: "picture_XCH7oGMGpUytmj71rLdl",
+  //   fileType: "image",
+  // });
 
-  return NextResponse.json({ downloadUrl }, { status: 200 });
+  return NextResponse.json({ message: "This is a test api" }, { status: 200 });
 }
